285b13d28cf9ddcf3a455768fb412e386dcc85b6
julien.bianchi <julien.bianchi@pmsipilot.com>
2013-12-19 10:27:34 +0100

    Use printenv to avoid overwriting env vars

--- src/driver/git.sh
+++ src/driver/git.sh
@@ -1,8 +1,4 @@
-BASHELOR_GIT_CMD=
-if [ -z "$BASHELOR_GIT_CMD" ]
-then
-	BASHELOR_GIT_CMD=git
-fi
+[ -z "$(printenv BASHELOR_GIT_CMD)" ] && BASHELOR_GIT_CMD=git
 
 function gitBashelorDriver() {
 	local GIT=$(which "$BASHELOR_GIT_CMD" 2> /dev/null)
