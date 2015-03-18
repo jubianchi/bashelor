1cb7d6d080435aaa033ccddd8d983081310468b5
jubianchi <julien.bianchi@pmsipilot.com>
2015-03-18 16:52:51 +0100

    Deprecated `inline` command

--- src/bashelor.sh
+++ src/bashelor.sh
@@ -8,7 +8,6 @@ function usage() {
 	log "Usage: $(success $0) $(warning [-h] [-q]) $(success [install] [inline])"
 	log
 	log "  $(success install): Install dependencies"
-	log "  $(success inline): Inline install dependencies"
 	log
 	log "  $(warning -q): Quiet mode (no output)"
 	log "  $(warning -h): Display this help message"
@@ -106,8 +105,6 @@ then
 
 		exit 2
 	fi
-
-	[ "$2" != "inline" ] && exit 0
 elif [ "$1" = "upgrade" ]
 then
 	upgrade
--- tests/specs/test-usage.sh
+++ tests/specs/test-usage.sh
@@ -12,13 +12,11 @@ it_displays_usage() {
 
 	local LINE1=$(echo "$OUTPUT" | head -n1)
 	local LINE3=$(echo "$OUTPUT" | head -n3 | tail -n1)
-	local LINE4=$(echo "$OUTPUT" | head -n4 | tail -n1)
+	local LINE5=$(echo "$OUTPUT" | head -n5 | tail -n1)
 	local LINE6=$(echo "$OUTPUT" | head -n6 | tail -n1)
-	local LINE7=$(echo "$OUTPUT" | head -n7 | tail -n1)
 
 	[ "$LINE1" = "Usage: $(success ../../bin/bashelor) $(warning [-h] [-q]) $(success [install] [inline])" ]
 	[ "$LINE3" = "  $(success install): Install dependencies" ]
-	[ "$LINE4" = "  $(success inline): Inline install dependencies" ]
-	[ "$LINE6" = "  $(warning -q): Quiet mode (no output)" ]
-	[ "$LINE7" = "  $(warning -h): Display this help message" ]
+	[ "$LINE5" = "  $(warning -q): Quiet mode (no output)" ]
+	[ "$LINE6" = "  $(warning -h): Display this help message" ]
 }
