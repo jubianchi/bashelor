cbd3b0f108d2a73ec0a02f9e41cdbb646a7fc27c
jubianchi <contact@jubianchi.fr>
2016-06-06 17:53:16 +0200

    Use which if available and fallback to whereis

--- src/driver/git.sh
+++ src/driver/git.sh
@@ -1,9 +1,7 @@
-[ -z "$(printenv BASHELOR_GIT_CMD)" ] && BASHELOR_GIT_CMD=git
+[ -z "$(printenv BASHELOR_GIT_CMD)" ] && BASHELOR_GIT_CMD=$(findCli "git" 2> /dev/null)
 
 function gitBashelorDriver() {
-	local GIT=$(which "$BASHELOR_GIT_CMD" 2> /dev/null)
-
-	if [[ -z "$GIT" || ! $? -eq 0 ]]
+	if [[ -z "$BASHELOR_GIT_CMD" || ! -x "$BASHELOR_GIT_CMD" ]]
 	then
 		error "git command ($BASHELOR_GIT_CMD) is not available"
 
@@ -17,26 +15,26 @@ function gitBashelorDriver() {
 
 	if [ -d "$DIRECTORY" ]
 	then
-		PREV_REV=$(${GIT} --git-dir="$DIRECTORY/.git" rev-parse --short HEAD)
+		PREV_REV=$(${BASHELOR_GIT_CMD} --git-dir="$DIRECTORY/.git" rev-parse --short HEAD)
 
 		log "=> Updating $(success "$DIRECTORY")"
 		(
 			cd "$DIRECTORY" && \
-			${GIT} pull
+			${BASHELOR_GIT_CMD} pull
 		) > /dev/null 2>&1
 	else
 		log "=> Cloning $(success "$URL") into $(success "$DIRECTORY")"
-		${GIT} clone "$URL" "$DIRECTORY" > /dev/null 2>&1
+		${BASHELOR_GIT_CMD} clone "$URL" "$DIRECTORY" > /dev/null 2>&1
 	fi
 
-	CURRENT_REV=$(${GIT} --git-dir="$DIRECTORY/.git" rev-parse --short HEAD)
+	CURRENT_REV=$(${BASHELOR_GIT_CMD} --git-dir="$DIRECTORY/.git" rev-parse --short HEAD)
 
 	if [ -n "$PREV_REV" ]
 	then
 		if [ "$PREV_REV" != "$CURRENT_REV" ]
 		then
 			warning "   Updated $PREV_REV..$CURRENT_REV"
-			warning "  $(${GIT} --git-dir="$DIRECTORY/.git" diff --shortstat "$PREV_REV..$CURRENT_REV")"
+			warning "  $(${BASHELOR_GIT_CMD} --git-dir="$DIRECTORY/.git" diff --shortstat "$PREV_REV..$CURRENT_REV")"
 		else
 			warning "   Nothing to update ($CURRENT_REV)"
 		fi
